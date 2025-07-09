52aeae126b6051fe13986bf9cf31083977fe8d94
Pinzon250 <santiagopinzo@gmail.com>
2025-07-09 01:08:55 -0500

    Estilos del footer

--- frontend/src/components/static/Footer.tsx
+++ frontend/src/components/static/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="py-6">
+    <footer className="py-6 border-t border-gray-200 mt-10">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-4 px-4">
         <div className="m-auto flex items-center gap-4">
           <p className="text-gray-500 font-normal text-center">
