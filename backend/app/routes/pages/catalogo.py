2c5a7d1a679d0d176407b3912a03b194e3685903
Pinzon250 <santiagopinzo@gmail.com>
2025-07-09 01:07:01 -0500

    Se añadieron endpoints para listar productos y obtener detalles de un producto en el catálogo; se implemento filtros por categoría, precio y búsqueda.

---
+++ backend/app/routes/pages/catalogo.py
@@ -0,0 +1,77 @@
+# Importar FastAPI
+from fastapi import APIRouter, Depends, Query, HTTPException
+
+# Sesiones con la base de datos
+from sqlalchemy.orm import Session
+
+# Tipos de datos
+from typing import Optional, List
+from decimal import Decimal
+
+# Importar schemas, base de datos
+from app.database.connection import get_db
+from app.models.producto import Producto
+from app.models.categoria import Categoria
+from app.schemas.producto import ProductoOut, ListProducts
+
+# Tag y Ruta inicial
+router = APIRouter(
+    prefix="/catalogo",
+    tags=["Catalogo"]
+)
+
+# Endpoint para obtener todos los productos
+@router.get("/productos", response_model=List[ListProducts])
+# Crear funcion con los parametros de filstros de: Categoria, precio minimo y maximo, marca, busquedas y limites de productos obtenidos
+def listar_productos(
+    db: Session = Depends(get_db),
+    categoria_nombre: Optional[str] = Query(None),
+    precio_min: Optional[float] = Query(None),
+    precio_max: Optional[float] = Query(None),
+    # Agregar Marca
+    marca: Optional[str] = Query(None),
+    search: Optional[str] = Query(None),
+    skip: int = 0,
+    limit: int = 20
+):
+    # Obtener los productos
+    query = db.query(Producto)
+
+    # Filtrar por categoria
+    if categoria_nombre:
+        query = query.join(Producto.categoria).filter(Categoria.nombre.ilike(f"%{categoria_nombre}%"))
+
+    # Filtrar por precio minimo
+    if precio_min is not None:
+        print("precio_min:", precio_min)
+        query = query.filter(Producto.precio >= Decimal(precio_min))
+    
+    # Filtrar por precio maximo
+    if precio_max is not None:
+        query = query.filter(Producto.precio <= Decimal(precio_max))
+
+    # Busqueda
+    if search:
+        query = query.filter(Producto.nombre.ilike(f"%{search}"))
+
+    
+    # Filtrar los productos
+    productos = query.offset(skip).limit(limit).all()
+
+    # Retornar productos
+    return productos
+
+# Obtener detalles de el producto
+@router.get("/productos/{producto_id}", response_model=ProductoOut)
+def detalle_producto(
+    producto_id: int,
+    db: Session = Depends(get_db)
+):
+    # Obtener producto por el id
+    producto = db.query(Producto).filter(Producto.id == producto_id).first()
+
+    # Excepcion por si no lo encuentra
+    if not producto:
+        raise HTTPException(status_code= 404, detail= "Producto no encontrado")
+    
+    return producto
